6e404f1e0edf23d00cfb0d2d4c09e103814561b2
시현 <@DESKTOP-1FQIKO7>
2023-06-28 00:03:54 +0900

    0628

--- cms/src/main/java/com/application/cms/admin/member/controller/AdminMemberController.java
+++ cms/src/main/java/com/application/cms/admin/member/controller/AdminMemberController.java
@@ -1,5 +1,22 @@
 package com.application.cms.admin.member.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.application.cms.admin.member.service.AdminMemberService;
+
+@Controller
+@RequestMapping("/admin/member")
 public class AdminMemberController {
 
+	@Autowired
+	private AdminMemberService adminMemberService;
+	
+	@GetMapping("/adminLogin")
+	public ModelAndView adminLogin() throws Exception {
+		return new ModelAndView("/admin/member/adminLogin");
+	}
 }
